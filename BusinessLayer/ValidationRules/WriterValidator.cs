dcd8fd957182a40563c6d6be2aa01f9b2e634b5c
yavuzcetin34 <yavuzcetin34@gmail.com>
2021-10-24 21:08:07 +0300

    35 FluentValidation

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,29 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+	public class WriterValidator : AbstractValidator<Writer>
+	{
+		public WriterValidator()
+		{
+			RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı soyadı boş olamaz ");
+			RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail adrsi boş olamaz ");
+			RuleFor(x => x.WriterPassord).NotEmpty().WithMessage("Şifre boş olamaz ");
+			RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("En az 2 karakter olmalı");
+			RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("En fazla 50 karakter olmalı");
+
+			//RuleFor(x => x.WriterPassord)
+			//		.MinimumLength(4).WithMessage("Şifre en az 4 karakter olmalı")
+			//		.Matches("[A-Z]").WithMessage("Şifrenizde büyük harf olmalı")
+			//		.Matches("[a-z]").WithMessage("Şifrenizde küçük harf olmalı")
+			//		.Matches("[0-9]").WithMessage("Şifrenizde rakam olmalı")
+			//		.Matches("[^a-zA-Z0-9]").WithMessage("Şifrenizde özel karakter olmalı");
+		}
+	}
+}
--- CoreDemo/Controllers/RegisterController.cs
+++ CoreDemo/Controllers/RegisterController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concreate;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
@@ -17,11 +19,27 @@ namespace CoreDemo.Controllers
         [HttpPost]
         public IActionResult Index(Writer writer)
         {
-            writer.WriterStatus = true;
-            writer.WriterAbout = "Deneme Test";
-            wm.AddWriter(writer);   
 
-            return RedirectToAction("Index","Blog");
+            WriterValidator wv = new WriterValidator();
+            wv.Validate(writer);
+
+            ValidationResult result = wv.Validate(writer);
+			if (result.IsValid)
+			{
+                writer.WriterStatus = true;
+                writer.WriterAbout = "Deneme Test";
+                wm.AddWriter(writer);
+
+                return RedirectToAction("Index", "Blog");
+			}
+			else
+			{
+				foreach (var item in result.Errors)
+				{
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+				}
+			}
+            return View();  
         }
 
     }
