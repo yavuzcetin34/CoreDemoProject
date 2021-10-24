d5fa68d3ead75462fe2fda9e79eb075cfffada6f
yavuzcetin34 <yavuzcetin34@gmail.com>
2021-10-24 20:48:42 +0300

    34

---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository()); 
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Writer writer)
+        {
+            writer.WriterStatus = true;
+            writer.WriterAbout = "Deneme Test";
+            wm.AddWriter(writer);   
+
+            return RedirectToAction("Index","Blog");
+        }
+
+    }
+}
