16007e56045930f29382fc4371bc4d6289747de2
yavuzcetin34 <yavuzcetin34@gmail.com>
2021-10-29 15:54:18 +0300

    51

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -20,5 +20,23 @@ namespace CoreDemo.Controllers
         {
             return View();
         }
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public PartialViewResult WriterNavbarPartial()
+        {
+            return PartialView();   
+        }
+
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+        {
+            return PartialView();
+        }
+
     }
 }
